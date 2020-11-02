e9852d52d77fa769e8615d455ad2067dce3b5400
Lydia Pitts <lydiapitts@dhcp-194-159.student-wlan.bowdoin.edu>
2020-11-01 19:36:36 -0500

    starting html display

--- poetry_generator.py
+++ poetry_generator.py
@@ -17,19 +17,10 @@ def make_limerick(ngram):
     limerick.evalutate()
     return limerick
 
-def make_top_limericks(ngram):
-    min_fitness = 2000
-    top_limericks = []
-    for number in range(5):
-        limerick = make_limerick(ngram)
-        top_limericks.append(limerick)
-        if limerick.fitness < min_fitness:
-            min_fitness = limerick.fitness
-    return top_limericks, min_fitness
 
 def get_limericks_above_100(ngram):
     top_limericks = []
-    while len(top_limericks) < 3:
+    while len(top_limericks) < 5:
         limerick = make_limerick(ngram)
         add_limerick = False
         if limerick.fitness > 100:
@@ -43,27 +34,9 @@ def get_limericks_above_100(ngram):
     return top_limericks
 
 
-def get_poems(ngram):
-    top_limericks, min_fitness = make_top_limericks(ngram)
-    for num in range(50):
-        limerick = make_limerick(ngram)
-        if limerick.fitness > min_fitness:
-            new_min = limerick.fitness
-            for i in range(5):
-                fit_num = top_limericks[i].fitness
-                if(fit_num == min_fitness):
-                    top_limericks[i] = limerick
-                if(fit_num < new_min):
-                    new_min = fit_num
-            min_fitness = new_min 
-    return top_limericks
-        
-
-
 def main():
     text = fr.read_file()
     ngram = fr.make_n_gram(text)
-    #top_poems = get_poems(ngram)
     top_poems = get_limericks_above_100(ngram)
     print("_________________________________________")
     for poem in top_poems:
